39cd3e554c8617acc9f821700710d3c9707060e0
Ryan King <ryanking3@users.noreply.github.com>
2026-07-26 00:58:30 +0100

    Replace PDF iframe with controlled renderer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 import "./globals.css";
 import "./polish.css";
 
